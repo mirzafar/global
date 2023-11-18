4690bd530630ae5ad4e927b873ac85b0aa7f7ea3
mirzafar <m_mirzafar@mail.ru>
2023-11-18 22:40:25 +0600

    fi

--- server/admin/api/lessons/__init__.py
+++ server/admin/api/lessons/__init__.py
@@ -7,14 +7,16 @@ from .list import LessonsView
 __all__ = ['lessons_bp']
 
 from .tags import LessonsTagsView, LessonsTagsItemView
+from .template import LessonsTemplateView
 
 lessons_bp = Blueprint('lessons', url_prefix='/lessons')
 
 # Lessons
-lessons_bp.add_route(LessonsCreateView.as_view(), '/create/', name='lessons.create')
-lessons_bp.add_route(LessonsView.as_view(), '/', name='lessons.list')
-lessons_bp.add_route(LessonsItemView.as_view(), '/<lesson_id>/', name='lessons.item')
+lessons_bp.add_route(LessonsCreateView.as_view(), '/create/')
+lessons_bp.add_route(LessonsView.as_view(), '/')
+lessons_bp.add_route(LessonsTemplateView.as_view(), '/template/')
+lessons_bp.add_route(LessonsItemView.as_view(), '/<lesson_id>/')
 
 # Tags
-lessons_bp.add_route(LessonsTagsView.as_view(), '/tags/', name='lessons.tags.list')
-lessons_bp.add_route(LessonsTagsItemView.as_view(), '/tags/<tag_id>/', name='lessons.tags.item')
+lessons_bp.add_route(LessonsTagsView.as_view(), '/tags/')
+lessons_bp.add_route(LessonsTagsItemView.as_view(), '/tags/<tag_id>/')
--- server/admin/api/lessons/list.py
+++ server/admin/api/lessons/list.py
@@ -1,29 +1,44 @@
+from sanic import response
+
 from core.db import db
+from core.encoder import encoder
 from core.handlers import BaseAPIView
 from core.pager import Pager
+from core.tools import set_counters
 from utils.lists import ListUtils
+from utils.strs import StrUtils
 
 
 class LessonsView(BaseAPIView):
-    template_name = 'admin/lessons.html'
 
     async def get(self, request, user):
         page = request.args.get('page', 1)
-        limit = request.args.get('limit', 50)
+        limit = request.args.get('limit', 3)
 
         pager = Pager()
         pager.set_page(page)
         pager.set_limit(limit)
+
+        query = StrUtils.to_str(request.args.get('query'))
+
+        cond, cond_vars = ['l.status >= {}'], [0]
+
+        if query:
+            cond.append('l.title ILIKE {}')
+            cond_vars.append(f'%{query}%')
+
+        cond, _ = set_counters(' AND '.join(cond))
 
         lessons = ListUtils.to_list_of_dicts(await db.fetch(
             '''
             SELECT 
                 l.*
             FROM public.lessons l
-            WHERE l.status >= 0
+            WHERE %s
             ORDER BY l.id DESC
             %s
-            ''' % pager.as_query()
+            ''' % (cond, pager.as_query()),
+            *cond_vars
         ))
 
         pager.total = await db.fetchval(
@@ -34,12 +49,10 @@ class LessonsView(BaseAPIView):
             '''
         ) or 0
 
-        self.context['data'] = {
+        self.context = {
+            '_success': True,
             'lessons': lessons,
             'pager': pager.dict()
         }
 
-        return self.render_template(
-            request=request,
-            user=user
-        )
+        return response.json(self.context, dumps=encoder.encode)
