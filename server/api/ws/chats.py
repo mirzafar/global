c394683000d10275ba6b99926adf25f94ab69588
mirzafar <m_mirzafar@mail.ru>
2023-11-22 17:18:13 +0600

    fix

--- server/api/ws/chats.py
+++ server/api/ws/chats.py
@@ -1,49 +1,28 @@
 import ujson
 from sanic import Request, Websocket
 
-from utils.lists import ListUtils
 from utils.strs import StrUtils
 
-rooms = dict()
-
-
-def get_chat_id(chat_keys) -> str:
-    try:
-        user_id, recipient_id = chat_keys
-        return f'{user_id}:{recipient_id}' if user_id > recipient_id else f'{recipient_id}:{user_id}'
-
-    except (Exception,):
-        return str()
+users = dict()
 
 
 async def chat_messages(request: Request, ws: Websocket):
     while True:
         data = ujson.loads(await ws.recv())
-        chat_keys = ListUtils.to_list_of_strs(data.get('chat_keys'))
-        chat_id = get_chat_id(chat_keys)
-        if not chat_id:
-            continue
-
+        user_id = StrUtils.to_str(data.get('user_id'))
         action = StrUtils.to_str(data.pop('action', None))
-        users = rooms[chat_id] if chat_id in rooms else []
-        if action == 'open':
-            if ws not in users:
-                users.append(ws)
 
-            rooms[chat_id] = users
+        if action == 'open':
+            if user_id:
+                users[user_id] = ws
 
         elif action in ['close', 'error']:
-            if ws in users:
-                users.remove(ws)
-
-            if users:
-                rooms[chat_id] = users
-            else:
-                rooms.pop(chat_id, None)
+            if user_id:
+                users.pop(user_id, None)
 
         elif action == 'message':
             data['is_me'] = False
-
-            for user in users:
-                if not user == ws:
-                    await user.send(ujson.dumps(data))
+            recipient_id = StrUtils.to_str(data.get('recipient_id'))
+            if recipient_id and recipient_id in users:
+                if not users[recipient_id] == ws:
+                    await users[recipient_id].send(ujson.dumps(data))
